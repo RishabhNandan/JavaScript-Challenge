33564894e859f2724d76555dbc12231d240c527e
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:51:35 +0530

    javascriptqus7.js

--- javascriptqus7.js
+++ javascriptqus7.js
@@ -1,14 +1,12 @@
-//Question - Write a program which enter marks in three subject of a students and calculate its average, independing on the average
-	//check and print the division according to following rule.
+Question - Write a program which enter marks in three subject of a students and calculate its average, independing on the average
+	check and print the division according to following rule.
 	
-	//Average                      Division
-	//60+                           First
-	//45 - <60                      Second
-	//33 - <45                      third
-	//<33                           Fails
-
-
-let subject1 = 70; 
+	Average                      Division
+	60+                           First
+	45 - <60                      Second
+	33 - <45                      third
+	<33                           Fails
+Solve:-let subject1 = 70; 
 let subject2 = 65; 
 let subject3 = 80; 
 let average = (subject1 + subject2 + subject3) / 3;
