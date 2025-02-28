5ecc6ced00355743be3d58ccce5e459170f01b24
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 23:22:45 +0530

    javascriptqus6.js

--- javascriptqus6.js
+++ javascriptqus6.js
@@ -1,9 +1,9 @@
  Question - WAP which enter sales made by any salesman than calculate and print the commision, according to following rules - 
-                                             Sale                      Commission
- 				upto 25000                5% of sales
-				25001 - 50000             7% of sales
- 				50001 - 75000             8% of sales
- 				75001 +                   10% of sales
+  Sale                      Commission
+ upto 25000                5% of sales
+25001 - 50000             7% of sales
+50001 - 75000             8% of sales
+75001 +                   10% of sales
            
 Solve:-let sales = 60000;
 let commission;
