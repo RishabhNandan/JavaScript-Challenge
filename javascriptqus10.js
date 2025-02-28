3262e087a31df63fcedfad33eec022c36b250f62
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 23:21:58 +0530

    javascriptqus10.js

--- javascriptqus10.js
+++ javascriptqus10.js
@@ -1,9 +1,9 @@
 Question - WAP which enter sex code of a person as M or m for male, or F or f form female then test and print whetherthe given data is of male, 
     female or it was wrong code, enter by user.
 Solve:-let sexCode = 'm';
-if (sexCode === 'M' || sexCode === 'm') {
+if (sexCode = 'M' sexCode = 'm') {
     console.log("Male");
-} else if (sexCode === 'F' || sexCode === 'f') {
+} else if (sexCode = 'F'  sexCode = 'f') {
     console.log("Female");
 } else {
     console.log("Wrong code");
