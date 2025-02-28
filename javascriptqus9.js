fc59a77a4206759d25e6bd7ccf9df8596201e475
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:49:19 +0530

    javascriptqus9.js

--- javascriptqus9.js
+++ javascriptqus9.js
@@ -1,7 +1,5 @@
-//Question - Write a program which enter any code in integer and if it is 5,7,11 or 24 then print 
- //Right code otherwise print wrong code.
-
-let code = 11;
+Question - Write a program which enter any code in integer and if it is 5,7,11 or 24 then print Right code otherwise print wrong code.
+Solve:-let code = 11;
 if (code === 5 || code === 7 || code === 11 || code === 24) {
     console.log("Right code");
 } else {
@@ -9,7 +7,7 @@ if (code === 5 || code === 7 || code === 11 || code === 24) {
 }
 
 //OR
-// Input the code
+Stap:-(2)
 let code = 10;
 if (code === 5 || code === 7 || code === 11 || code === 24) {
     console.log("Right code");
