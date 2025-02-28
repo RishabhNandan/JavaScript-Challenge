437ca715d0fc01b99637157029f82f7f2c3950fb
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:54:10 +0530

    javascriptqus10.js

--- javascriptqus10.js
+++ javascriptqus10.js
@@ -1,6 +1,6 @@
-//Question - WAP which enter sex code of a person as M or m for male, or F or f form female then test and print whether
-//the given data is of male, female or it was wrong code, enter by user.
-let sexCode = 'm';
+Question - WAP which enter sex code of a person as M or m for male, or F or f form female then test and print whetherthe given data is of male, 
+    female or it was wrong code, enter by user.
+Solve:-let sexCode = 'm';
 if (sexCode === 'M' || sexCode === 'm') {
     console.log("Male");
 } else if (sexCode === 'F' || sexCode === 'f') {
