87a8db493337c3ffc7732b25f9bb7509a70525cd
Rishabh Nandan <161109704+RishabhNandan@users.noreply.github.com>
2025-02-28 16:53:27 +0530

    javascriptqus6.js

--- javascriptqus6.js
+++ javascriptqus6.js
@@ -1,13 +1,11 @@
-// Question - WAP which enter sales made by any salesman than calculate and print the commision, according
-// 				to following rules - 
-// 				Sale                      Commission
-// 				upto 25000                5% of sales
-// 				25001 - 50000             7% of sales
-// 				50001 - 75000             8% of sales
-// 				75001 +                   10% of sales
+ Question - WAP which enter sales made by any salesman than calculate and print the commision, according to following rules - 
+                                             Sale                      Commission
+ 				upto 25000                5% of sales
+				25001 - 50000             7% of sales
+ 				50001 - 75000             8% of sales
+ 				75001 +                   10% of sales
            
-let sales = 60000;
-
+Solve:-let sales = 60000;
 let commission;
 if (sales <= 25000) {
     commission = sales * 0.05; 
